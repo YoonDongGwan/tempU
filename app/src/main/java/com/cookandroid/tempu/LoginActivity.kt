57b6348b0e30dfcbb04f39bee9321a927d49b5c5
yundonggwan <ydg3825@gmail.com>
2021-12-28 21:11:41 +0900

    메인 액티비티와 연결된 액티비티들 생성

--- app/src/main/java/com/cookandroid/tempu/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/tempu/LoginActivity.kt
@@ -14,6 +14,7 @@ class LoginActivity : AppCompatActivity() {
         loginBtn.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/cookandroid/tempu/MainActivity.kt
+++ app/src/main/java/com/cookandroid/tempu/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.cookandroid.tempu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todayTemp.setOnClickListener(onClickListener)
+        todayBet.setOnClickListener(onClickListener)
+        tempRecord.setOnClickListener(onClickListener)
+        hallOfFame.setOnClickListener(onClickListener)
+    }
+
+    private val onClickListener = View.OnClickListener { v ->
+        when (v?.id) {
+            R.id.todayTemp -> {
+                intent = Intent(applicationContext, TodayTempActivity::class.java)
+            }
+            R.id.todayBet ->{
+                intent = Intent(applicationContext, TodayBetActivity::class.java)
+            }
+            R.id.tempRecord ->{
+                intent = Intent(applicationContext, TempRecordActivity::class.java)
+            }
+            R.id.hallOfFame ->{
+                intent = Intent(applicationContext, HallOfFameActivity::class.java)
+            }
+        }
+        startActivity(intent)
     }
 }
