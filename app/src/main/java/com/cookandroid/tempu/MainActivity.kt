82f007e27d7458a176ff2993a8e25aca845b5d64
hgfdsa4320 <hgfdsa4320@naver.com>
2021-12-28 16:29:11 +0900

    asd

--- app/src/main/java/com/cookandroid/tempu/MainActivity.kt
+++ app/src/main/java/com/cookandroid/tempu/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cookandroid.tempu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val change_nickname_btn=findViewById<ImageButton>(R.id.change_nickname_btn);
+        change_nickname_btn.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/cookandroid/tempu/NicknameChange.java
@@ -0,0 +1,7 @@
+package com.cookandroid.tempu;
+
+import android.content.Context;
+
+public class NicknameChange(context:Context, Interface: CustomDialogInterface) : Dialog(context) {
+    private var customDialogInterface: CustomDialogInterface = Interface
+}
