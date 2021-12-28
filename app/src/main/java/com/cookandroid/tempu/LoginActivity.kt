21b9ba189c76b8feded6afedd185609f443dd7be
yundonggwan <ydg3825@gmail.com>
2021-12-28 17:52:43 +0900

    재업로드

--- app/src/main/java/com/cookandroid/tempu/LoginActivity.kt
+++ app/src/main/java/com/cookandroid/tempu/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.cookandroid.tempu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
