23c3f759ccbe5f19b471632cfaebbf22ac83e34e
hgfdsa4320 <hgfdsa4320@naver.com>
2022-01-06 00:13:27 +0900

    오늘의 베팅 레이아웃 구현

--- app/src/main/java/com/cookandroid/tempu/TodayTempActivity.kt
+++ app/src/main/java/com/cookandroid/tempu/TodayTempActivity.kt
@@ -1,11 +1,27 @@
 package com.cookandroid.tempu
 
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_today_temp.*
 
 class TodayTempActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_today_temp)
+
+        today_temp_submit_btn.setOnClickListener(onClickListener)
+    }
+
+    private val onClickListener = View.OnClickListener { v ->
+        when (v?.id) {
+            R.id.today_temp_submit_btn -> {
+                progressBar.setProgress(Integer.parseInt(edit_temp.text.toString()))
+            }
+        }
     }
 }
